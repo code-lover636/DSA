44193d75252dd8986db4e71368bca3ae774045c5
Aravind Ashokan <77882744+code-lover636@users.noreply.github.com>
2023-09-28 09:53:44 +0530

    Update histogram.c

--- Cycle1/histogram.c
+++ Cycle1/histogram.c
@@ -8,21 +8,35 @@ Output:t-3, h-1,i-3,s-4,’ ‘-4…….etc
 #include <stdio.h>
 #include <string.h>
 
+int In(char ch, char string[]){
+    for(int i=0; i<strlen(string); i++){
+        if(string[i]==ch)
+            return 1;
+    }
+    return 0;
+}
+
 int main() {
-    char histogram[] = "histogram";
-    char str[100];
+    char str[100], duplicate[100];
     printf("Input string: ");
     scanf("%[^\n]s", &str);
     
     // Calculating and printing
-    int count, i, j;
-    for(i=0; i<strlen(histogram); i++){
+    int count, i, j, k=0;
+    for(i=0; i<strlen(str); i++){
         count = 0;
+        // Checking if char already counted
+        if(In(str[i],duplicate))
+            continue;
+        else
+            duplicate[k++]=str[i];
+        // Finding the count
         for(j=0; j<strlen(str); j++){
-            if(histogram[i]==str[j])
+            if(str[i]==str[j]){
                 count++;
+            }
         }
-        printf("%c=%d\n",histogram[i],count);
+        printf("%c=%d\n",str[i],count);
     }
 }
 
