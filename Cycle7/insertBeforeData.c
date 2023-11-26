5b7130f0ec1dec9c73ec0152bce5b14887692d4d
Aravind Ashokan <77882744+code-lover636@users.noreply.github.com>
2023-11-26 20:10:00 +0530

    Update insertBeforeData.c

--- Cycle7/insertBeforeData.c
+++ Cycle7/insertBeforeData.c
@@ -5,43 +5,54 @@ struct Node{
 	int data;
 	struct Node *next;
 };
+struct Node *head = NULL;
 
-
-int add(struct Node *head, int data){
+int add(int data){
 	struct Node *n = head;
 	struct Node *node = malloc(sizeof(struct Node*));
 	node -> data = data;
-	while(n->next != NULL){
-		n = n->next;
+	
+	if(head==NULL){
+		head = node;
 	}
-	n->next = node;
+	else{
+		while(n->next != NULL){
+			n = n->next;
+		}
+		n->next = node;
+	}
 }
 
-void insert(struct Node *head, int data, int key){
+void insert(int data, int key){
 	struct Node *n = head;
 	struct Node *node = malloc(sizeof(struct Node*));
 	node -> data = data;
-	while(n->next != NULL){
-		n = n->next;
+	
+	if(n->data == key){
+		node->next = head;
+		head = node;
+		return;	
+	}
+	while(n != NULL){
 		if(n->next->data == key){
 			node->next = n->next;
 			n->next = node;
 			return;
 		}
+		n = n->next;
 	}
 	printf("key is not found\n");
 }
 
-int print(struct Node *head){
+int print(){
 	struct Node *n = head;
-	while(n->next != NULL){
-		n = n->next;
+	while(n != NULL){
 		printf("%d\t", n->data);
+		n = n->next;
 	}
 }
 
 int main() {
-	struct Node *head = malloc(sizeof(struct Node*));
 	int n, elem, key;
 
 	printf("Enter size of list: ");
@@ -50,17 +61,17 @@ int main() {
 	for(int i=0; i<n; i++){
 		printf("Enter number: ");
 		scanf("%d", &elem);
-		add(head, elem);
+		add(elem);
 	}	
-	print(head);
+	print();
 	printf("\n");
 	
 	printf("Enter key data: ");
 	scanf("%d", &key);
 	printf("Enter data to insert: ");
 	scanf("%d", &elem);
-	insert(head, elem, key);
+	insert(elem, key);
 	
-	print(head);
+	print();
 	printf("\n");
 }
